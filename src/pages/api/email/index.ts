eb82cfb7ef7879387de77d8214839d69c5e55f85
Raniel Brown <developer@ranielbrown.com>
2022-02-20 14:37:32 -0500

    make subtle changes

--- src/pages/api/email/index.ts
+++ src/pages/api/email/index.ts
@@ -1,63 +1,67 @@
-import { students } from "./constants"
-import { arrayToJson, ConvertToCSV, csvToBase64 } from "./functions"
-import sgMail from "@sendgrid/mail"
-import * as functions from "firebase-functions"
+import { students } from "./constants";
+import { arrayToJson, ConvertToCSV, csvToBase64 } from "./functions";
+import sgMail from "@sendgrid/mail";
+import * as functions from "firebase-functions";
 
 // import * as functions from "firebase-functions"
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const handleError = (res: functions.Response, message: string, status = 400) => {
-  res.statusCode = status
-  res.json({ status: "error", message })
-}
+const handleError = (
+  res: functions.Response,
+  message: string,
+  status = 400
+) => {
+  res.statusCode = status;
+  res.json({ status: "error", message });
+};
 
 // function to handle convert json to csv and send it to the client using cloud function
 const handleEmail = functions.https.onRequest(async (req, res) => {
   if (!req.body || req.method !== "POST") {
-    return handleError(res as functions.Response, "Invalid request")
+    return handleError(res as functions.Response, "Invalid request");
   }
   // convert the array to json
-  const convertedArr = arrayToJson(students)
+  const convertedArr = arrayToJson(students);
   //Convert the json to csv
-  const csv = await ConvertToCSV(JSON.parse(convertedArr))
+  const csv = await ConvertToCSV(JSON.parse(convertedArr));
   try {
-    //Cloud function to send the email
-    const email = req.body.email
-    const subject = req.body.subject
+    //Collecting the email and subject line from the request
+    const email = req.body.email;
+    const subject = req.body.subject;
 
+    // Using the sendgrid api to send the email
     await sgMail.send({
       to: email,
       from: process.env.SENDGRID_FROM,
       subject,
-      text: "Please find the attached CSV file",
+      text: "Below is an attachment of the requested csv file",
       attachments: [
         {
           content: csvToBase64(csv),
           filename: "students.csv",
           type: "text/csv",
           disposition: "attachment",
-          contentId: "my-custom-file",
         },
       ],
-    })
+    });
 
     res.json({
       status: "success",
       message: "Email sent successfully",
-    })
+    });
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.error(error)
+    console.error(error);
     res.status(500).json({
       status: "error",
       message: "Email not sent",
       error,
-    })
+    });
   } finally {
-    res.end()
+    res.end();
   }
   //Export sendEmail function to firebase cloud function
-})
+});
 
-export default handleEmail
+export default handleEmail;
